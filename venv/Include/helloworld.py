531a9af0d0ec1976aa571d5644e38f9cad637b1c
Oksana <necheporenko30@gmail.com>
2020-05-31 22:38:29 +0300

    hometask 6 by Necheporenko

--- venv/Include/helloworld.py
+++ venv/Include/helloworld.py
@@ -1,14 +1,11 @@
+import random
+arg1 = int(input('Введите длину списка: '))
+arg2 = int(input('Введите максимальное значение элемента списка: '))
 
-a=int(input('Введите число 1: '))
-b=int(input('Введите число 2: '))
-v=int(input('Введите число 3: '))
+def create_list(arg1,arg2):
+    new_list = list()
+    while(len(new_list) < arg1):
+        new_list.append(random.randint(0,arg2 + 1))
+    print(new_list)
 
-def cycle(a,b,v):
-    while a <= b:
-        print(str(a) + ' Пока что нет')
-        a = a + v
-    else:
-        a > b
-        print('Дождались ' + str(a))
-
-cycle(a,b,v)
+create_list(arg1,arg2)
