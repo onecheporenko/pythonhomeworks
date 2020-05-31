9df09f2b9e51f101072f252736298e80f04e0044
Oksana <necheporenko30@gmail.com>
2020-05-31 21:58:04 +0300

    hometask 5 by Necheporenko

--- venv/Include/helloworld.py
+++ venv/Include/helloworld.py
@@ -1,26 +1,14 @@
 
-
 a=int(input('Введите число 1: '))
 b=int(input('Введите число 2: '))
-c= int(input('Введите число 3: '))
-if a > b:
-    print("Свершилось!")
-elif b > a :
-   print("Да ну!")
-elif b == a :
-   print("А если так?")
-   a = a+b
-   b = b-c
-   if a > b:
-       print("Свершилось!")
-   elif b > a:
-       print("Да ну!")
-   elif b == a:
-       print("А если так?")
+v=int(input('Введите число 3: '))
 
+def cycle(a,b,v):
+    while a <= b:
+        print(str(a) + ' Пока что нет')
+        a = a + v
+    else:
+        a > b
+        print('Дождались ' + str(a))
 
-
-
-
-
-
+cycle(a,b,v)
